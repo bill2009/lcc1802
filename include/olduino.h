1b1fa39eea46f7e7ab3f0021d16da66ca3f2392b
bill.rowe.ottawa <9397866+bill2009@users.noreply.github.com>
2013-11-13 22:33:08 +0000

    source code for the Rhinestone Compiler

--- include/olduino.h
+++
@@ -1,6 +0,0 @@
-void delay(unsigned int);
-void digitalWrite(unsigned char,unsigned char);
-int digitalRead(unsigned char); //nb, always reads ef3
-unsigned char PIN4=0; //used to save what's written to the output port
-#define LOW 0
-#define HIGH 1
