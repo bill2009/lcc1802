554b6f801dfff8026673872302281247a22071c0
bill.rowe.ottawa <9397866+bill2009@users.noreply.github.com>
2014-07-06 17:50:53 +0000

    cleanup

--- examples/helloworld/helloworld7.c
+++ examples/helloworld/helloworld7.c
@@ -1,21 +1,10 @@
 /*
    print the string "hello World!"
-   output is via port 7 instead of 5
 */
-//Jan 29 - trying to define putc(x) as out(7,x)
 #include <nstdlib.h>
 #define putc(x) out(7,x)
 void main()
 {
 	printstr("Hello World!\n");
-
-	while(1){
-		printstr("@@");
-		asm("	seq\n");
-		out(7,'*');
-		asm("	req\n");
-		out(7,'!');
-	}
-
 }
 #include <nstdlib.c>
