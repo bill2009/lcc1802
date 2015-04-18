76e88760fb8378dbcb6c7a9533122cb3ac4c2eed
bill.rowe.ottawa <9397866+bill2009@users.noreply.github.com>
2015-04-18 18:09:25 +0000

    double dabble incorporating test for last byte<10 and special processing

--- examples/dubdab/muledd32cr1.c
+++ examples/dubdab/muledd32cr1.c
@@ -2,19 +2,38 @@
 #include "nstdlib.h"
 #include <cpu1802spd4port7.h>
 char * dd32cr1(long int);
-void inc(){
-	asm("\tinclude dd32cr1.inc\n");
+char * ultoa(unsigned long int,char *);
+char * xltoa(unsigned long binval, char* bcd){
+	//if (binval>9){
+		asm("\tinclude ddgt32.inc\n"); //ends with a return
+	//}else{
+	//	bcd[0]=binval|'0';
+	//	bcd[1]=0;
+	//}
+	return bcd;
 }
 void main(){
-	char *bcd;long binval=12345;
+	char bcdbuf[12]={10,10,10,10,10,10,10,10,10,10,10,10};
+	char *bcd;unsigned long binval=21; unsigned int bvali=12345;
+	int i;
+
 	delay(3000);
-	printf("\rBCD Mule Saturday\r\n");
-	printf("%ld ",binval);
+	printf("\rBCD Mule Thursday\r\n");
+/*
+	printf("16 bit %d\r\n",bvali);
+	printf(" 32 bit %ld ",binval);
 	printf("%lx ",binval);
-	printf("%lx ",binval);
-	bcd=dd32cr1(binval);
-	printf("%x ",bcd);
-	printf("%cx %cx %cx %cx %cx %cx %cx %cx %cx %cx",bcd[0],bcd[1],bcd[2],bcd[3],bcd[4],bcd[5],bcd[6],bcd[7],bcd[8],bcd[9]);
+	bcd=ultoa(binval,bcdbuf);
+	printf("buffer is at %x\r\n",bcdbuf);
+	printf("%x:",bcd);
+	printf("%cx %cx %cx %cx %cx %cx %cx %cx %cx %cx %cx %cx",bcd[0],bcd[1],bcd[2],bcd[3],bcd[4],bcd[5],bcd[6],bcd[7],bcd[8],bcd[9],bcd[10],bcd[11]);
+	printf("\n\rresult %s\n\r",bcd);
+*/
+	for (i=10;i>=-10;i--){
+		binval=i;
+		printf("%d %lx=%ld=%s\n\r",i,binval,binval,xltoa(binval,bcdbuf));
+		binval--;
+	}
 }
 #include "nstdlib.c"
 #include "olduino.c"
