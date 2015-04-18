171570fe9e1e183a5c8209a7f6aac716a0e97bbb
bill.rowe.ottawa@gmail.com <bill.rowe.ottawa@gmail.com@eb17a4d8-bb3f-b7f4-9369-459629264cf3>
2015-04-18 18:51:29 +0000

    converts both 32 bit and 16 bit unsigned ints to ascii

--- trunk/examples/dubdab/muledd32cr1.c
+++ trunk/examples/dubdab/muledd32cr1.c
@@ -3,14 +3,9 @@
 #include <cpu1802spd4port7.h>
 char * dd32cr1(long int);
 char * ultoa(unsigned long int,char *);
-char * xltoa(unsigned long binval, char* bcd){
-	//if (binval>9){
-		asm("\tinclude ddgt32.inc\n"); //ends with a return
-	//}else{
-	//	bcd[0]=binval|'0';
-	//	bcd[1]=0;
-	//}
-	return bcd;
+char * uitoa(unsigned int,char *);
+void	xtoainc(){
+	asm("\tinclude ddgt32.inc\n"); //ends with a return
 }
 void main(){
 	char bcdbuf[12]={10,10,10,10,10,10,10,10,10,10,10,10};
@@ -18,20 +13,10 @@ void main(){
 	int i;
 
 	delay(3000);
-	printf("\rBCD Mule Thursday\r\n");
-/*
-	printf("16 bit %d\r\n",bvali);
-	printf(" 32 bit %ld ",binval);
-	printf("%lx ",binval);
-	bcd=ultoa(binval,bcdbuf);
-	printf("buffer is at %x\r\n",bcdbuf);
-	printf("%x:",bcd);
-	printf("%cx %cx %cx %cx %cx %cx %cx %cx %cx %cx %cx %cx",bcd[0],bcd[1],bcd[2],bcd[3],bcd[4],bcd[5],bcd[6],bcd[7],bcd[8],bcd[9],bcd[10],bcd[11]);
-	printf("\n\rresult %s\n\r",bcd);
-*/
-	for (i=10;i>=-10;i--){
+	printf("\rBCD Mule Saturday\r\n");
+	for (i=10;i>=-1;i--){
 		binval=i;
-		printf("%d %lx=%ld=%s\n\r",i,binval,binval,xltoa(binval,bcdbuf));
+		printf("%d %lx=%ld=%s=%s\n\r",i,binval,binval,ultoa(binval,bcdbuf),uitoa(i,bcdbuf));
 		binval--;
 	}
 }
