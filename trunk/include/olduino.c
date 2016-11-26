1370c10f3e7b74c474cdf92fabfd25982571a47e
bill2009 <bill2009@eb17a4d8-bb3f-b7f4-9369-459629264cf3>
2016-11-26 15:29:31 +0000

    changed oneMs routine in epilog to calculate loop count from LCC1802CPUSPEED changed olduino.c to use oneMs instead of oneMsBN and to set LCC1802CPUSPEED to 4000000 if __CPUSPEED4 is set for backward compatability

--- trunk/include/olduino.c
+++ trunk/include/olduino.c
@@ -2,22 +2,17 @@
 //Feb 7 2013, changed digitalwrite to out4 instead of out5
 //Feb 26 added digitalRead on ef3
 //Nov 9 2014 added oneMsBN to soak up one ms adjusted for 4mhz clock
+//16-11-26 removed OneMsBN in favour of oneMs in epilog
+//cpu speed is set by LCC1802CPUSPEED assembler variable
 void delay(unsigned int howlong){
 	unsigned int i;
 	for (i=1;i!=howlong;i++){
-		oneMsBN();
+		oneMs();
 	}
 }
 void olduinoincluder(){
 	asm("\tinclude olduino.inc\n");
-	asm("	align 8\n"
-		"_oneMsBN:		;execute enough instructions  for 1 ms\n");
-	#ifndef __CPUSPEED4
-	asm("	ldi	(100-15-10-2)/2\n"); //for 1.6mhz 1802
-	#else
-	asm("	ldi	(250-15-10-2)/2\n"); //for 4mhz 1802
-	#endif
-	asm("$$mslp: smi	1\n"
-		"	bnz	$$mslp\n"
-		"	Cretn\n");
+#ifndef __CPUSPEED4
+	asm("LCC1802CPUSPEED set 4000000"); //for 4mhz 1802
+#endif
 }
