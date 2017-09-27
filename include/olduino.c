e100072b29b70d093ba83087240d65cf363558fb
bill2009 <bill2009@eb17a4d8-bb3f-b7f4-9369-459629264cf3>
2017-09-27 19:51:15 +0000

    avoid duplicate setting of cpu speed

--- include/olduino.c
+++ include/olduino.c
@@ -4,6 +4,7 @@
 //Nov 9 2014 added oneMsBN to soak up one ms adjusted for 4mhz clock
 //16-11-26 removed OneMsBN in favour of oneMs in epilog
 //cpu speed is set by LCC1802CPUSPEED assembler variable
+//17-4-26 hack to avoid double setting of cpu speed
 void delay(unsigned int howlong){
 	unsigned int i;
 	for (i=1;i!=howlong;i++){
@@ -13,6 +14,8 @@ void delay(unsigned int howlong){
 void olduinoincluder(){
 	asm("\tinclude olduino.inc\n");
 #ifdef __CPUSPEED4
-	asm("LCC1802CPUSPEED set 4000000"); //for 4mhz 1802
+	asm(" ifndef LCC1802CPUSPEED\n"); //for 4mhz 1802
+	asm("LCC1802CPUSPEED set 4000000\n"); //for 4mhz 1802
+	asm(" endif\n"); //for 4mhz 1802
 #endif
 }
