fb85ac0eb862889ac6ec85e71e9d385f17bc74d2
bill2009 <bill2009@eb17a4d8-bb3f-b7f4-9369-459629264cf3>
2018-01-25 14:55:36 +0000

    simplified

--- trunk/examples/blink/blink2.c
+++ trunk/examples/blink/blink2.c
@@ -1,7 +1,6 @@
 /*
     blink the q led
 */
-#include "nstdlib.h"
 #include "olduino.h"
 void mysetqOn(){
 	asm("	seq\n");
@@ -10,22 +9,11 @@ void main()
 {
 	unsigned char flippy=0;
 	while(1){
-		out(4,flippy);
-		if (flippy==0){
-			flippy=0xff;
-		}else {
-			flippy=0;
-		}
-		//out(7,'!');
-		mysetqOn();
+		setqOn();
 		delay(250);
 		setqOff();
 		delay(250);
-		mysetqOn();
-		delay(250);
-		setqOff();
-		delay(250);
-		mysetqOn();
+		setqOn();
 		delay(250);
 		setqOff();
 		delay(1000);
@@ -33,4 +21,3 @@ void main()
 }
 
 #include "olduino.c" //for the delay routine
-#include <nstdlib.c> //for printf
