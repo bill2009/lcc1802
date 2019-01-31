31a894c71e3f4cfb1bf1bf9dbf9151300d71e83d
bill2009 <bill2009@eb17a4d8-bb3f-b7f4-9369-459629264cf3>
2019-01-31 16:06:15 +0000

    updating build instructions, adding buildvs17.bat and makefile.linux(provided by Mark Abene)

---
+++ lcc42/buildvs17.bat
@@ -0,0 +1,11 @@
+set BUILDDIR=c:\lcc42\bin
+pushd .
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+popd
+nmake -f makefile.nt all
+rem nmake /G /G /P -f makefile.nt HOSTFILE=etc/win32.c lcc 
+
+rem   to compile "ops" gcc  -Isrc etc\ops.c
+rem to run it ops c=1 s=2 i=2 l=4 h=4 f=4 d=8 x=8 p=2
+
+pause
