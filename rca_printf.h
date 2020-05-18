403d3e6ae9d35e8a6234c17abe625bd9eade817c
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2020-05-18 23:10:41 +0200

    Added rca_printf and editorial changes to rca_system

---
+++ rca_printf.h
@@ -0,0 +1,13 @@
+#ifndef _RCA_PRINTF_H
+#define _RCA_PRINTF_H
+
+//rca_printf header
+void putlcccx(unsigned char c);
+void gotoxy(unsigned char x, unsigned char y);
+void rca_printf_includer(){
+asm(" include rca_printf.inc\n");
+}
+
+#define putc(c) putlcccx(c)
+
+#endif // __RCA_PRINTF_H
---
+++ rca_system.h
@@ -0,0 +1,11 @@
+#ifndef _RCA_SYSTEM_H
+#define _RCA_SYSTEM_H
+
+//rca_system header
+void disableinterrupt();
+void enableinterrupt();
+void rca_system_includer(){
+asm(" include rca_system.inc\n");
+}
+
+#endif // _RCA_SYSTEM_H
