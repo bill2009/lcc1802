a4ada6b49b1c1c9fee34a03c5e3fca9ac680f81d
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2020-05-21 13:47:44 +0200

    Added TMC600 support

--- include/rca_vis_video.h
+++ include/rca_vis_video.h
@@ -3,14 +3,21 @@
 
 //rca_vis_video header
 void setvideobase(unsigned short int vidmem);
+#ifdef TMC600
 void vidchar(unsigned short int vidmem, unsigned char character);
 void vidstrcpy(unsigned short int vidmem, char * text);
 void vidclr(unsigned int vidmem, int vidlen);
+unsigned char bgcolor(unsigned char color);
+void textcolor(unsigned char color);
+#else
+void vidchar(unsigned short int vidmem, unsigned char character);
+void vidstrcpy(unsigned short int vidmem, char * text);
 void shapechar(const unsigned char * shapelocation, int number);
 void shapecolor(unsigned short int character, unsigned char number, unsigned char color);
 unsigned char bgcolor(unsigned char color);
 void textcolordefinition(unsigned char definition);
 void monochrome(unsigned char mono);
+#endif
 void rca_vis_video_includer(){
 asm(" include rca_vis_video.inc\n");
 }
