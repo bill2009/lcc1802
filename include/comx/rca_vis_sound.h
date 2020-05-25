734bb8712ff88dede027054985d1cacc2ff1c26a
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2020-05-25 23:14:59 +0200

    Small optimisation, rewrote PECOM joystick routines, fixed PECOM bug in come_final.inc

--- include/comx/rca_vis_sound.h
+++ include/comx/rca_vis_sound.h
@@ -3,7 +3,9 @@
 
 //rca_vis_sound header
 void generatetone(unsigned char tone, unsigned char range, unsigned char volume);
+// tone is using max 7 bits, range max 3 bits and volume 4 bits
 void generatenoise(unsigned char range, unsigned char volume);
+// range is using max 3 bits and volume 4 bits
 void rca_vis_sound_includer(){
 asm(" include comx/rca_vis_sound.inc\n");
 }
