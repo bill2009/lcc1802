3d16c414ba682c24424099da4d78a1c54f9019ae
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-06-08 00:52:48 +0200

    Rename joy macro constants to avoid conflicts with CrossLib

--- include/comx/rca_joystick.h
+++ include/comx/rca_joystick.h
@@ -4,35 +4,35 @@
 //rca_keyboard_encoder header
 
 #ifdef __COMX__
-#define JOY_UP 0x82
-#define JOY_RIGHT 0x83
-#define JOY_LEFT 0x84
-#define JOY_DOWN 0x85
-#define JOY_FIRE 0x5f
+#define MOVE_UP 0x82
+#define MOVE_RIGHT 0x83
+#define MOVE_LEFT 0x84
+#define MOVE_DOWN 0x85
+#define MOVE_FIRE 0x5f
 #endif
 
 #ifdef __PECOM__
-#define JOY_UP 0x5e
-#define JOY_RIGHT 0x5d
-#define JOY_LEFT 0x5c
-#define JOY_DOWN 0x5b
-#define JOY_FIRE 0x40
+#define MOVE_UP 0x5e
+#define MOVE_RIGHT 0x5d
+#define MOVE_LEFT 0x5c
+#define MOVE_DOWN 0x5b
+#define MOVE_FIRE 0x40
 #endif
 
 #ifdef __TMC600__
-#define JOY_UP 0xb
-#define JOY_RIGHT 0x9
-#define JOY_LEFT 0x8
-#define JOY_DOWN 0xa
-#define JOY_FIRE 0x20
+#define MOVE_UP 0xb
+#define MOVE_RIGHT 0x9
+#define MOVE_LEFT 0x8
+#define MOVE_DOWN 0xa
+#define MOVE_FIRE 0x20
 #endif
 
 #ifdef __CIDELSA__
-#define JOY_UP 0x8
-#define JOY_RIGHT 0x4
-#define JOY_LEFT 0x2	
-#define JOY_DOWN 0x10
-#define JOY_FIRE 0x20
+#define MOVE_UP 0x8
+#define MOVE_RIGHT 0x4
+#define MOVE_LEFT 0x2	
+#define MOVE_DOWN 0x10
+#define MOVE_FIRE 0x20
 #endif
 
 unsigned char get_stick();
