298829d403f17a872df27066f32a966e7dfc63f9
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2020-06-20 22:04:11 +0200

    Microboard text colour fixes

--- include/devkit/video/comx_char.c
+++ include/devkit/video/comx_char.c
@@ -29,22 +29,25 @@ void character_set(unsigned char number)
 			shapechar(shape_rom, 0x00, 1);
 			shapechar(shape_rom, 0x20, 1);
 			shape_rom = (unsigned char *)0x7b5;
-			shapechar(shape_rom, 0x30, NUMBER_COLOR1+0xa);
+			shapechar(shape_rom, 0x30, CHAR_COLOR1+0xa);
 			shape_rom = (unsigned char *)0x84e;
-			shapechar(shape_rom, 0x41, ALPHA_COLOR1+0x1a);
+			shapechar(shape_rom, 0x41, CHAR_COLOR1+0x1a);
 #elif __PECOM__
 			shape_rom = (unsigned char *)0x8800;
 			shapechar(shape_rom, 0x700, 1);
 			shapechar(shape_rom, 0x720, 1);
 			shape_rom = (unsigned char *)0x8990;
-			shapechar(shape_rom, 0x730, NUMBER_COLOR1+0xa);
+			shapechar(shape_rom, 0x730, CHAR_COLOR1+0xa);
 			shape_rom = (unsigned char *)0x8a07;
-			shapechar(shape_rom, 0x741, ALPHA_COLOR1+0x1a);
+			shapechar(shape_rom, 0x741, CHAR_COLOR1+0x1a);
 #else
-			shapechar(shape_space, 0x00, 1);
+			shapechar(shape_at, 0x00, CHAR_COLOR1+1);
 			shapechar(shape_space, 0x20, 1);
-			shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
-			shapechar(shape_alpha, 0x41, ALPHA_COLOR1+0x1a);
+			shapechar(shape_sym1, 0x21, CHAR_COLOR1+15);
+			shapechar(shape_numbers, 0x30, CHAR_COLOR1+0xa);
+			shapechar(shape_sym2, 0x3a, CHAR_COLOR1+6);
+			shapechar(shape_alpha, 0x41, CHAR_COLOR1+0x1a);
+			shapechar(shape_sym3, 0x5b, CHAR_COLOR1+5);
 #endif
 		break;
 
@@ -55,27 +58,33 @@ void character_set(unsigned char number)
 			shapechar(shape_rom, 0x00, 1);
 			shapechar(shape_rom, 0x20, 1);
 			shape_rom = (unsigned char *)0x7b5;
-			shapechar(shape_rom, 0x30, NUMBER_COLOR1+0xa);
+			shapechar(shape_rom, 0x30, CHAR_COLOR1+0xa);
 			shape_rom = (unsigned char *)0x84e;
-			shapechar(shape_rom, 0x41, ALPHA_COLOR1+0x1a);
+			shapechar(shape_rom, 0x41, CHAR_COLOR1+0x1a);
 #elif __PECOM__
 			shape_rom = (unsigned char *)0x8800;
 			shapechar(shape_rom, 0x700, 1);
 			shapechar(shape_rom, 0x720, 1);
 			shape_rom = (unsigned char *)0x8990;
-			shapechar(shape_rom, 0x730, NUMBER_COLOR1+0xa);
+			shapechar(shape_rom, 0x730, CHAR_COLOR1+0xa);
 			shape_rom = (unsigned char *)0x8a07;
-			shapechar(shape_rom, 0x741, ALPHA_COLOR1+0x1a);
+			shapechar(shape_rom, 0x741, CHAR_COLOR1+0x1a);
 #else
-			shapechar(shape_space, 0x00, 1);
+			shapechar(shape_at, 0x00, CHAR_COLOR1+1);
 			shapechar(shape_space, 0x20, 1);
-			shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
-			shapechar(shape_alpha, 0x41, ALPHA_COLOR1+0x1a);
+			shapechar(shape_sym1, 0x21, CHAR_COLOR1+15);
+			shapechar(shape_numbers, 0x30, CHAR_COLOR1+0xa);
+			shapechar(shape_sym2, 0x3a, CHAR_COLOR1+6);
+			shapechar(shape_alpha, 0x41, CHAR_COLOR1+0x1a);
+			shapechar(shape_sym3, 0x5b, CHAR_COLOR1+5);
 #ifdef __NTSC2_9__
-			shapechar(shape_space, 0x80, 1);
+			shapechar(shape_at, 0x80, CHAR_COLOR2+1);
 			shapechar(shape_space, 0xA0, 1);
-			shapechar(shape_numbers, 0xB0, NUMBER_COLOR2+0xa);
-			shapechar(shape_alpha, 0xC1, ALPHA_COLOR2+0x1a);
+			shapechar(shape_sym1, 0xA1, CHAR_COLOR2+15);
+			shapechar(shape_numbers, 0xB0, CHAR_COLOR2+0xa);
+			shapechar(shape_sym2, 0xBA, CHAR_COLOR2+6);
+			shapechar(shape_alpha, 0xC1, CHAR_COLOR2+0x1a);
+			shapechar(shape_sym3, 0xDB, CHAR_COLOR2+5);
 #endif
 #endif
 		break;
@@ -90,11 +99,11 @@ void character_set(unsigned char number)
 			shapechar(shape_rom, 0x40, 1);
 			shapechar(shape_rom, 0x60, 1);
 			shape_rom = (unsigned char *)0x7b5;
-			shapechar(shape_rom, 0x30, NUMBER_COLOR1+0xa);
-			shapechar(shape_rom, 0x70, NUMBER_COLOR2+0xa);
+			shapechar(shape_rom, 0x30, CHAR_COLOR1+0xa);
+			shapechar(shape_rom, 0x70, CHAR_COLOR2+0xa);
 			shape_rom = (unsigned char *)0x84e;
-			shapechar(shape_rom, 0x01, ALPHA_COLOR1+0x1a);
-			shapechar(shape_rom, 0x41, ALPHA_COLOR2+0x1a);
+			shapechar(shape_rom, 0x01, CHAR_COLOR1+0x1a);
+			shapechar(shape_rom, 0x41, CHAR_COLOR2+0x1a);
 #elif __PECOM__
 			shape_rom = (unsigned char *)0x8800;
 			shapechar(shape_rom, 0x700, 1);
@@ -102,29 +111,41 @@ void character_set(unsigned char number)
 			shapechar(shape_rom, 0x740, 1);
 			shapechar(shape_rom, 0x760, 1);
 			shape_rom = (unsigned char *)0x8990;
-			shapechar(shape_rom, 0x730, NUMBER_COLOR1+0xa);
-			shapechar(shape_rom, 0x770, NUMBER_COLOR2+0xa);
+			shapechar(shape_rom, 0x730, CHAR_COLOR1+0xa);
+			shapechar(shape_rom, 0x770, CHAR_COLOR2+0xa);
 			shape_rom = (unsigned char *)0x8a07;
-			shapechar(shape_rom, 0x701, ALPHA_COLOR1+0x1a);
-			shapechar(shape_rom, 0x741, ALPHA_COLOR2+0x1a);
+			shapechar(shape_rom, 0x701, CHAR_COLOR1+0x1a);
+			shapechar(shape_rom, 0x741, CHAR_COLOR2+0x1a);
 #else
-			shapechar(shape_space, 0x00, 1);
+			shapechar(shape_at, 0x00, CHAR_COLOR1+1);
 			shapechar(shape_space, 0x20, 1);
-			shapechar(shape_alpha, 0x01, ALPHA_COLOR1+0x1a);
-			shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
-			shapechar(shape_space, 0x40, 1);
+			shapechar(shape_alpha, 0x01, CHAR_COLOR1+0x1a);
+			shapechar(shape_sym3, 0x1B, CHAR_COLOR1+5);
+			shapechar(shape_sym1, 0x21, CHAR_COLOR1+15);
+			shapechar(shape_numbers, 0x30, CHAR_COLOR1+0xa);
+			shapechar(shape_sym2, 0x3a, CHAR_COLOR1+6);
+			shapechar(shape_at, 0x40, CHAR_COLOR2+1);
 			shapechar(shape_space, 0x60, 1);
-			shapechar(shape_alpha, 0x41, ALPHA_COLOR2+0x1a);
-			shapechar(shape_numbers, 0x70, NUMBER_COLOR2+0xa);
+			shapechar(shape_alpha, 0x41, CHAR_COLOR2+0x1a);
+			shapechar(shape_sym3, 0x5B, CHAR_COLOR2+5);
+			shapechar(shape_sym1, 0x61, CHAR_COLOR2+15);
+			shapechar(shape_numbers, 0x70, CHAR_COLOR2+0xa);
+			shapechar(shape_sym2, 0x7a, CHAR_COLOR2+6);
 #ifdef __NTSC2_9__
-			shapechar(shape_space, 0x80, 1);
+			shapechar(shape_at, 0x80, CHAR_COLOR3+1);
 			shapechar(shape_space, 0xA0, 1);
-			shapechar(shape_alpha, 0x81, ALPHA_COLOR3+0x1a);
-			shapechar(shape_numbers, 0xB0, NUMBER_COLOR3+0xa);
-			shapechar(shape_space, 0xC0, 1);
+			shapechar(shape_alpha, 0x81, CHAR_COLOR3+0x1a);
+			shapechar(shape_sym3, 0x9B, CHAR_COLOR3+5);
+			shapechar(shape_sym1, 0xA1, CHAR_COLOR3+15);
+			shapechar(shape_numbers, 0xB0, CHAR_COLOR3+0xa);
+			shapechar(shape_sym2, 0xBA, CHAR_COLOR3+6);
+			shapechar(shape_at, 0xC0, CHAR_COLOR4+1);
 			shapechar(shape_space, 0xE0, 1);
-			shapechar(shape_alpha, 0xC1, ALPHA_COLOR4+0x1a);
-			shapechar(shape_numbers, 0xF0, NUMBER_COLOR4+0xa);
+			shapechar(shape_alpha, 0xC1, CHAR_COLOR4+0x1a);
+			shapechar(shape_sym3, 0xDB, CHAR_COLOR4+5);
+			shapechar(shape_sym1, 0xE1, CHAR_COLOR4+15);
+			shapechar(shape_numbers, 0xF0, CHAR_COLOR4+0xa);
+			shapechar(shape_sym2, 0xFA, CHAR_COLOR4+6);
 #endif
 #endif
 		break;
