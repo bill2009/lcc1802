b6f65342553f02a81802eb61cd031c0e5f86c766
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2020-06-24 16:49:24 +0200

    Editorial fix

--- include/devkit/video/cidelsa_char.c
+++ include/devkit/video/cidelsa_char.c
@@ -1,7 +1,7 @@
 //
 // *******************************************************************
 // *** This software is copyright 2020 by Marcel van Tongeren      ***
-// *** with support from Bill Rowe and Fabrizio Caruso.			   ***
+// *** with support from Bill Rowe and Fabrizio Caruso.            ***
 // ***                                                             ***
 // *** You have permission to use, modify, copy, and distribute    ***
 // *** this software so long as this copyright notice is retained. ***
@@ -16,49 +16,49 @@
 #ifdef __CIDELSA__
 void character_set(unsigned char number)
 {
-	number--;
-	switch (number&3)
-	{
-		case 0:
-			vis_text_color_mask = 0xff;
-			shapechar(shape_space, 0x00, 1);
-			shapechar(shape_space, 0x20, 1);
-			shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
-			shapechar(shape_alpha, 0x41, ALPHA_COLOR1+0x1a);
-		break;
+    number--;
+    switch (number&3)
+    {
+        case 0:
+            vis_text_color_mask = 0xff;
+            shapechar(shape_space, 0x00, 1);
+            shapechar(shape_space, 0x20, 1);
+            shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
+            shapechar(shape_alpha, 0x41, ALPHA_COLOR1+0x1a);
+        break;
 
-		case 1:
-			vis_text_color_mask = 0x7f;
-			shapechar(shape_space, 0x00, 1);
-			shapechar(shape_space, 0x20, 1);
-			shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
-			shapechar(shape_alpha, 0x41, ALPHA_COLOR1+0x1a);
-			shapechar(shape_space, 0x80, 1);
-			shapechar(shape_space, 0xA0, 1);
-			shapechar(shape_numbers, 0xB0, NUMBER_COLOR2+0xa);
-			shapechar(shape_alpha, 0xC1, ALPHA_COLOR2+0x1a);
-		break;
+        case 1:
+            vis_text_color_mask = 0x7f;
+            shapechar(shape_space, 0x00, 1);
+            shapechar(shape_space, 0x20, 1);
+            shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
+            shapechar(shape_alpha, 0x41, ALPHA_COLOR1+0x1a);
+            shapechar(shape_space, 0x80, 1);
+            shapechar(shape_space, 0xA0, 1);
+            shapechar(shape_numbers, 0xB0, NUMBER_COLOR2+0xa);
+            shapechar(shape_alpha, 0xC1, ALPHA_COLOR2+0x1a);
+        break;
 
-		case 2:
-		case 3:
-			vis_text_color_mask = 0x3f;
-			shapechar(shape_space, 0x00, 1);
-			shapechar(shape_space, 0x20, 1);
-			shapechar(shape_alpha, 0x01, ALPHA_COLOR1+0x1a);
-			shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
-			shapechar(shape_space, 0x40, 1);
-			shapechar(shape_space, 0x60, 1);
-			shapechar(shape_alpha, 0x41, ALPHA_COLOR2+0x1a);
-			shapechar(shape_numbers, 0x70, NUMBER_COLOR2+0xa);
-			shapechar(shape_space, 0x80, 1);
-			shapechar(shape_space, 0xA0, 1);
-			shapechar(shape_alpha, 0x81, ALPHA_COLOR3+0x1a);
-			shapechar(shape_numbers, 0xB0, NUMBER_COLOR3+0xa);
-			shapechar(shape_space, 0xC0, 1);
-			shapechar(shape_space, 0xE0, 1);
-			shapechar(shape_alpha, 0xC1, ALPHA_COLOR4+0x1a);
-			shapechar(shape_numbers, 0xF0, NUMBER_COLOR4+0xa);
-		break;
-	}
+        case 2:
+        case 3:
+            vis_text_color_mask = 0x3f;
+            shapechar(shape_space, 0x00, 1);
+            shapechar(shape_space, 0x20, 1);
+            shapechar(shape_alpha, 0x01, ALPHA_COLOR1+0x1a);
+            shapechar(shape_numbers, 0x30, NUMBER_COLOR1+0xa);
+            shapechar(shape_space, 0x40, 1);
+            shapechar(shape_space, 0x60, 1);
+            shapechar(shape_alpha, 0x41, ALPHA_COLOR2+0x1a);
+            shapechar(shape_numbers, 0x70, NUMBER_COLOR2+0xa);
+            shapechar(shape_space, 0x80, 1);
+            shapechar(shape_space, 0xA0, 1);
+            shapechar(shape_alpha, 0x81, ALPHA_COLOR3+0x1a);
+            shapechar(shape_numbers, 0xB0, NUMBER_COLOR3+0xa);
+            shapechar(shape_space, 0xC0, 1);
+            shapechar(shape_space, 0xE0, 1);
+            shapechar(shape_alpha, 0xC1, ALPHA_COLOR4+0x1a);
+            shapechar(shape_numbers, 0xF0, NUMBER_COLOR4+0xa);
+        break;
+    }
 }
 #endif
--- include/devkit/video/comx_char.c
+++ include/devkit/video/comx_char.c
@@ -1,7 +1,7 @@
 //
 // *******************************************************************
 // *** This software is copyright 2020 by Marcel van Tongeren      ***
-// *** with support from Bill Rowe and Fabrizio Caruso.			   ***
+// *** with support from Bill Rowe and Fabrizio Caruso.            ***
 // ***                                                             ***
 // *** You have permission to use, modify, copy, and distribute    ***
 // *** this software so long as this copyright notice is retained. ***
@@ -16,81 +16,81 @@
 void character_set(unsigned char number)
 {
 #if defined __COMX__ || defined __PECOM__
-	unsigned char *shape_rom;
+    unsigned char *shape_rom;
 #endif
-	number--;
-	number = number &3;
-	switch (number)
-	{
-		case 0:
-			vis_text_color_mask = 0xff;
+    number--;
+    number = number &3;
+    switch (number)
+    {
+        case 0:
+            vis_text_color_mask = 0xff;
 #ifdef __COMX__
-			shape_rom = (unsigned char *)0x725;
-			shapechar(shape_rom, 0x20, CHAR_COLOR1+32);
-			shape_rom = (unsigned char *)0x845;
-			shapechar(shape_rom, 0x40, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x725;
+            shapechar(shape_rom, 0x20, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x845;
+            shapechar(shape_rom, 0x40, CHAR_COLOR1+32);
 #elif __PECOM__
-			shape_rom = (unsigned char *)0x8920;
-			shapechar(shape_rom, 0x720, CHAR_COLOR1+32);
-			shape_rom = (unsigned char *)0x8a00;
-			shapechar(shape_rom, 0x740, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x8920;
+            shapechar(shape_rom, 0x720, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x8a00;
+            shapechar(shape_rom, 0x740, CHAR_COLOR1+32);
 #else
-			shapechar(shapes_comx_32, 0x20, CHAR_COLOR1+32);
-			shapechar(shapes_comx_0, 0x40, CHAR_COLOR1+32);
+            shapechar(shapes_comx_32, 0x20, CHAR_COLOR1+32);
+            shapechar(shapes_comx_0, 0x40, CHAR_COLOR1+32);
 #endif
-		break;
+        break;
 
-		case 1:
-			vis_text_color_mask = 0x7f;
+        case 1:
+            vis_text_color_mask = 0x7f;
 #ifdef __COMX__
-			shape_rom = (unsigned char *)0x725;
-			shapechar(shape_rom, 0x20, CHAR_COLOR1+32);
-			shape_rom = (unsigned char *)0x845;
-			shapechar(shape_rom, 0x40, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x725;
+            shapechar(shape_rom, 0x20, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x845;
+            shapechar(shape_rom, 0x40, CHAR_COLOR1+32);
 #elif __PECOM__
-			shape_rom = (unsigned char *)0x8920;
-			shapechar(shape_rom, 0x720, CHAR_COLOR1+32);
-			shape_rom = (unsigned char *)0x8a00;
-			shapechar(shape_rom, 0x740, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x8920;
+            shapechar(shape_rom, 0x720, CHAR_COLOR1+32);
+            shape_rom = (unsigned char *)0x8a00;
+            shapechar(shape_rom, 0x740, CHAR_COLOR1+32);
 #else
-			shapechar(shapes_comx_32, 0x20, CHAR_COLOR1+32);
-			shapechar(shapes_comx_0, 0x40, CHAR_COLOR1+32);
+            shapechar(shapes_comx_32, 0x20, CHAR_COLOR1+32);
+            shapechar(shapes_comx_0, 0x40, CHAR_COLOR1+32);
 #ifdef __NTSC2_9__
-			shapechar(shapes_comx_32, 0xA0, CHAR_COLOR2+32);
-			shapechar(shapes_comx_0, 0xC0, CHAR_COLOR2+32);
+            shapechar(shapes_comx_32, 0xA0, CHAR_COLOR2+32);
+            shapechar(shapes_comx_0, 0xC0, CHAR_COLOR2+32);
 #endif
 #endif
-		break;
+        break;
 
-		case 2:
-		case 3:
-			vis_text_color_mask = 0x3f;
+        case 2:
+        case 3:
+            vis_text_color_mask = 0x3f;
 #ifdef __COMX__
-			shape_rom = (unsigned char *)0x725;
-			shapechar(shape_rom, 0x20, CHAR_COLOR1+32);
-			shapechar(shape_rom, 0x60, CHAR_COLOR2+32);
-			shape_rom = (unsigned char *)0x845;
-			shapechar(shape_rom, 0x00, CHAR_COLOR1+32);
-			shapechar(shape_rom, 0x40, CHAR_COLOR2+32);
+            shape_rom = (unsigned char *)0x725;
+            shapechar(shape_rom, 0x20, CHAR_COLOR1+32);
+            shapechar(shape_rom, 0x60, CHAR_COLOR2+32);
+            shape_rom = (unsigned char *)0x845;
+            shapechar(shape_rom, 0x00, CHAR_COLOR1+32);
+            shapechar(shape_rom, 0x40, CHAR_COLOR2+32);
 #elif __PECOM__
-			shape_rom = (unsigned char *)0x8920;
-			shapechar(shape_rom, 0x720, CHAR_COLOR1+32);
-			shapechar(shape_rom, 0x760, CHAR_COLOR2+32);
-			shape_rom = (unsigned char *)0x8a00;
-			shapechar(shape_rom, 0x700, CHAR_COLOR1+32);
-			shapechar(shape_rom, 0x740, CHAR_COLOR2+32);
+            shape_rom = (unsigned char *)0x8920;
+            shapechar(shape_rom, 0x720, CHAR_COLOR1+32);
+            shapechar(shape_rom, 0x760, CHAR_COLOR2+32);
+            shape_rom = (unsigned char *)0x8a00;
+            shapechar(shape_rom, 0x700, CHAR_COLOR1+32);
+            shapechar(shape_rom, 0x740, CHAR_COLOR2+32);
 #else
-			shapechar(shapes_comx_32, 0x20, CHAR_COLOR1+32);
-			shapechar(shapes_comx_32, 0x60, CHAR_COLOR2+32);
-			shapechar(shapes_comx_0, 0x00, CHAR_COLOR1+32);
-			shapechar(shapes_comx_0, 0x40, CHAR_COLOR2+32);
+            shapechar(shapes_comx_32, 0x20, CHAR_COLOR1+32);
+            shapechar(shapes_comx_32, 0x60, CHAR_COLOR2+32);
+            shapechar(shapes_comx_0, 0x00, CHAR_COLOR1+32);
+            shapechar(shapes_comx_0, 0x40, CHAR_COLOR2+32);
 #ifdef __NTSC2_9__
-			shapechar(shapes_comx_32, 0xA0, CHAR_COLOR3+32);
-			shapechar(shapes_comx_32, 0xE0, CHAR_COLOR4+32);
-			shapechar(shapes_comx_0, 0x80, CHAR_COLOR3+32);
-			shapechar(shapes_comx_0, 0xC0, CHAR_COLOR4+32);
+            shapechar(shapes_comx_32, 0xA0, CHAR_COLOR3+32);
+            shapechar(shapes_comx_32, 0xE0, CHAR_COLOR4+32);
+            shapechar(shapes_comx_0, 0x80, CHAR_COLOR3+32);
+            shapechar(shapes_comx_0, 0xC0, CHAR_COLOR4+32);
 #endif
 #endif
-		break;
-	}
+        break;
+    }
 }
