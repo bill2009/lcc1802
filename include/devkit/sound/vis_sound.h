613336bec98dfe7bbc4428068a662504e038c286
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2021-02-02 22:32:54 +0100

    Changed to _t names from stdint.h

--- include/devkit/sound/vis_sound.h
+++ include/devkit/sound/vis_sound.h
@@ -5,9 +5,9 @@
 
 #include "devkit/video/vis.h"
 
-void generatetone(unsigned char tone, unsigned char range, unsigned char volume);
+void generatetone(uint8_t tone, uint8_t range, uint8_t volume);
 // tone is using max 7 bits, range max 3 bits and volume 4 bits
-void generatenoise(unsigned char range, unsigned char volume);
+void generatenoise(uint8_t range, uint8_t volume);
 // range is using max 3 bits and volume 4 bits
 void vis_sound_includer(){
 asm(" include devkit/sound/vis_sound.inc\n");
