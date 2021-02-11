08b3e9903cc5027e9146e74ab982407faeda5c8a
bill rowe <bill_rowe_ottawa@hotmail.com>
2021-02-11 17:03:54 -0500

    Update cabaPX.bat
    adding two more operands to the file so it can be run with:
    @..\..\..\bat\cabaPX pixie 1802 1760900 0 0x0EFF "-DRES=32" "-DVIDMEM=0x0F00" "-D__VIP__" pixie_32
    to create pixie32.hex

--- bat/cabaPX.bat
+++ bat/cabaPX.bat
@@ -8,11 +8,11 @@
 @if not "%5"=="" set stackloc=%5
 @echo compile %1.c for cpu %cpu% speed %speed% codeloc %codeloc% stackloc %stackloc%
 
-@c:\lcc42\bin\lcc.exe "-target=xr18CX" "-Wf-volatile" "-Wf-g,;" -DRES=32 -DVIDMEM=0x7F00 -D__VIP__ -S %6 %7 %1.c 
+@c:\lcc42\bin\lcc.exe "-target=xr18CX" "-Wf-volatile" "-Wf-g,;" -DRES=32 -DVIDMEM=0x7F00 -D__VIP__ -S %6 %7 %8 %1.c 
 @IF ERRORLEVEL 1 GOTO quit
 @c:\lcc42\bin\copt c:\lcc42\include\lcc1806.opt -v -I %1.asm -O %1.oasm
 @c:\lcc42\bin\asw -cpu %cpu% -quiet -P -x -i c:\lcc42\include -D CPUSPEED=%speed% -D CODELOC=%codeloc% -D STACKLOC=%stackloc% -D LCCPX=1 %1.oasm
 @c:\Python27\python c:\lcc42\bat\brfixPX.py %1.i
 @c:\lcc42\bin\asw -cpu %cpu%  -quiet -x -i c:\lcc42\include -L -D CPUSPEED=%speed% -D CODELOC=%codeloc% -D STACKLOC=%stackloc% -D LCCPX=1 %1.basm
-@c:\lcc42\bin\p2hex -r $-$ %1 %1 | more +2
+@c:\lcc42\bin\p2hex -r $-$ %1 %9 | more +2
 @:quit
