f3846a1a7c0baf79dd4fc44dd89c8999ae893312
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2021-02-12 22:36:44 +0100

    Extended with drawsprite routine

--- include/devkit/video/pixie_video.h
+++ include/devkit/video/pixie_video.h
@@ -6,13 +6,24 @@
 #include "stdint.h"
 #include "devkit/system/flags.h"
 
+void initvideo();
+void vidclr();
+#if defined PIXIE_SPRITE
+#define X_SIZE 64
+#define Y_SIZE RES
+void flags_pixie_sprite_includer(){
+asm("PIXIE_SPRITE equ 1\n");
+}
+void drawsprite(uint8_t x, uint8_t y, const uint8_t * spritedata);
+#endif
+#if defined PIXIE_TILE
 #define X_SIZE 16
 #define Y_SIZE 8
-
-void initvideo();
-void drawsprite(uint8_t x, uint8_t y, const uint8_t * spritedata);
+void flags_pixie_tile_includer(){
+asm("PIXIE_TILE equ 1\n");
+}
 void drawtile(uint8_t x, uint8_t y, const uint8_t * spritedata);
-void vidclr();
+#endif
 
 #define xstr(s) str(s)
 #define str(s) #s
