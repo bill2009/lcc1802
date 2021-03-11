8b0fb58d74b54beb1c472ef86aa63b85896343e7
Marcel van Tongeren <marcel.v.tongeren@gmail.com>
2021-03-11 21:45:16 +0100

    Latest pixie files

--- include/devkit/video/pixie_video.h
+++ include/devkit/video/pixie_video.h
@@ -6,15 +6,36 @@
 #include "stdint.h"
 #include "devkit/system/flags.h"
 
+#define PIXIE_SYNC 0x8000
+
+uint16_t pixie_wait_vsync;
+uint16_t pixie_video_mem;
+uint32_t pixie_sprite_data;
+
 void initvideo();
 void vidclr();
+#ifndef PIXIE_NO_VSYNC
+void flags_pixie_vsync_includer(){
+asm("PIXIE_VSYNC equ 1\n");
+}
+#endif
 #if defined PIXIE_SPRITE
 #define X_SIZE 64
 #define Y_SIZE RES
 void flags_pixie_sprite_includer(){
 asm("PIXIE_SPRITE equ 1\n");
 }
-void drawsprite(uint8_t x, uint8_t y, const uint8_t * spritedata);
+uint8_t drawsprite(uint8_t x, uint8_t y, const uint8_t * spriteshape);
+#endif
+#if defined PIXIE_SPRITE_MOVE
+#define X_SIZE 64
+#define Y_SIZE RES
+void flags_pixie_sprite_includer(){
+asm("PIXIE_SPRITE_MOVE equ 1\n");
+}
+uint8_t showsprite(uint32_t * spritedata, const uint8_t * spriteshape, uint8_t x, uint8_t y);
+uint8_t movesprite(uint32_t * spritedata, uint8_t direction);
+void removesprite(uint32_t * spritedata);
 #endif
 #if defined PIXIE_TILE
 #define X_SIZE 16
@@ -22,7 +43,7 @@ void drawsprite(uint8_t x, uint8_t y, const uint8_t * spritedata);
 void flags_pixie_tile_includer(){
 asm("PIXIE_TILE equ 1\n");
 }
-void drawtile(uint8_t x, uint8_t y, const uint8_t * spritedata);
+void drawtile(uint8_t x, uint8_t y, const uint8_t * spriteshape);
 #endif
 
 #define xstr(s) str(s)
