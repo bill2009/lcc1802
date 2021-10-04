b91c3d53f8b6c9848962a59bfbafbce70002f7af
bill rowe <bill_rowe_ottawa@hotmail.com>
2021-10-04 08:40:32 -0400

    changing as[] to separate parameters

--- lcc42/etc/linux.c
+++ lcc42/etc/linux.c
@@ -2,6 +2,7 @@
 //20-05-10 default version xr18CX
 //21-10-01 changing LCCDIR to LCCEXE for executables, defaulting to /lcc1802/ for includes
 //21-10-01 using ash.sh to invoke asl - otherwise it doesn't recognize parameters
+//21-10-04 changing as[] to separate parameters
 #include <string.h>
 
 static char rcsid[] = "$Id: linux.c,v 1.5 1998/09/16 20:41:09 drh Exp $";
@@ -18,7 +19,8 @@ char inputs[256] = "";
 char *include[] = {"-I" LCCDIR "include", "-I/usr/include", 0 };
 char *cpp[] = { LCCEXE "cpp", "-D__STRICT_ANSI__", "$1", "$2", "$3", 0 };
 char *com[] = { LCCEXE "rcc", "-target=xr18CX", "$1", "$2", "$3", 0 }; //wjr 20-18-10
-char *as[] = { "/lcc1802/ash.sh", "-cpu 1802", "-i " LCCDIR "include", "-L", "-quiet", "", "-o", "$3", "$1", "$2", 0 };
+char *as[] = { "/usr/bin/asl", "-cpu", "1802", "-i ", LCCDIR "include", "-L", /*"-quiet"*/, "", "-o", "$3", "$1", "$2", 0 };
+//char *as[] = { "/lcc1802/ash.sh", "-cpu 1802", "-i " LCCDIR "include", "-L", "-quiet", "", "-o", "$3", "$1", "$2", 0 };
 char *ld[] = { LCCEXE "p2hex", "", "", "", "", "$2", "$3", "","",0 }; //wjr dec 12
 char *peep[] = { LCCEXE "copt", LCCDIR "include/lcc1806.opt", "-I", "$2", "-O", "$3", 0 }; //#wjr 20-05-10
 
